b0d1a56a8480eb44d7a4ffec761ed3fc59c57088
blob <blob@lerp.io>
2025-03-06 14:33:25 -0500

    update build

--- app/books/[slug]/page.tsx
+++ app/books/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Books({ params }: { params: { slug: string } }) {
+export default function Books({ params }: any) {
 	return (
 		<div>
 			<h1>book details for {params.slug}</h1>
