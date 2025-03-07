1e41a4d098fed78e89b233821b8ae95ee55214b9
blob <blob@lerp.io>
2025-03-07 16:23:13 -0500

    add hints

--- app/BookListItem.tsx
+++ app/BookListItem.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import { Book } from "./publicEnums";
 import { getAssetURL } from "./getAssetURL";
 import { format } from 'date-fns'
-import { BookAIcon, BookIcon, BookOpenIcon, HeadphonesIcon, MonitorSmartphoneIcon } from "lucide-react";
+import { BookAIcon, BookIcon, BookOpenIcon, EllipsisIcon, HeadphonesIcon, MonitorSmartphoneIcon } from "lucide-react";
 import Link from "next/link";
 import { BookLinks } from "./BookLinkts";
 
 export function BookListItem({ book }: { book: Book }) {
 	return (
-		<div className="w-full py-8 px-8 border-main-600 border-solid border-r-1 border-b-1 items-start flex flex-col justify-start font-mono">
+		<div className="w-full py-8 px-8 border-main-600 border-solid border-r-1 border-b-1 items-start flex flex-col justify-start font-mono ">
 
-			<Link href={`/books/${book.id}`} className="bg-main-750 rounded-3xl overflow-hidden p-8  w-full h-[20em] flex items-center justify-center">
-				<img className='w-auto h-full min-w-[10em] rounded-md' src={getAssetURL(book.cover)} alt={book.book_title} />
+			<Link href={`/books/${book.id}`} className="hover:bg-main-500 bg-main-600 rounded-3xl flex-col overflow-hidden p-8  w-full h-[20em] flex items-center justify-center ring-4 ring-main-500">
+				<img className='max-h-[15em] max-w-full object-contain rounded-md' src={getAssetURL(book.cover)} alt={book.book_title} />
+
 			</Link>
 			<div className="ml-4 mt-3 text-white font-display text-md font-bold">{book.book_title}</div>
 			<div className="ml-4 mt-0 text-main-300 font-mono text-sm font-thin">{format(new Date(book.date), 'yyyy')}</div>
--- app/books/BookList.tsx
+++ app/books/BookList.tsx
@@ -12,7 +12,7 @@ export default async function BookList({ skip }: { skip?: number }) {
 	}
 
 	return (
-		<div className='grid grid-cols-1 md:grid-cols-3  lg:grid-cols-4 gap-0 '>
+		<div className='grid grid-cols-2 md:grid-cols-3  lg:grid-cols-4 gap-0 '>
 			{books.map((book) => <BookListItem key={book.id} book={book} />)}
 		</div>
 	);
