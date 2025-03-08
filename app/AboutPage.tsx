d2edf4af33b112f8e9d6d3b73a3e1a3ee96251bc
blob <blob@lerp.io>
2025-03-08 10:43:02 -0500

    update

--- app/AboutPage.tsx
+++ app/AboutPage.tsx
@@ -4,10 +4,10 @@ import { getData } from "./getData";
 export async function AboutPage() {
 	const { globals } = await getData()
 
-	return <div className='w-full flex flex-col md:flex-row gap-0 border-main-600 border-solid border-b-1 mb-20'>
+	return <div className='w-full flex flex-col md:flex-row gap-0 border-main-600 border-solid border-b-1 mb-20 items-stretch justify-evenly content-stretch'>
 
-		<div className='w-[20rem] shrink-0 h-full flex items-start justify-center py-10 relative border-main-600 border-solid border-r-1'>
-			<img className="h-[20em] rounded-xl" src={getAssetURL(globals.author_picture)} alt={globals.header_name} />
+		<div className='w-full md:w-[20rem] shrink-0 h-full flex items-center justify-center py-10 relative border-main-600 border-solid border-r-1'>
+			<img className="h-[15em] rounded-2xl " src={getAssetURL(globals.author_picture)} alt={globals.header_name} />
 		</div>
 		<div className='p-10  bg-main-800'>
 
