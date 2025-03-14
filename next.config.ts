5de5b4d7338be1f09a9b4766b136ce9a2ab5ab59
0xF48 <poofyiq@gmail.com>
2025-03-14 16:28:48 -0400

    update inline script

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ const nextConfig = {
 	images: {
 		domains: ['localhost', 'admin.janelcsterbentz.com'],
 	},
+	async headers() {
+		return [
+			{
+				source: '/:path*',
+				headers: [
+					{
+						key: 'Content-Security-Policy',
+						value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';",
+					},
+				],
+			},
+		];
+	},
 	// Add any other Next.js configuration you need
 };
 
