6e45a5e5c69eec8cc3996f27d19eef278e3e2ec9
0xF48 <poofyiq@gmail.com>
2025-07-21 11:47:09 -0400

    add back book page and add cors

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,15 @@ const nextConfig = {
 					},
 				],
 			},
+			{
+				source: '/:path*',
+				headers: [
+					{ key: 'Access-Control-Allow-Credentials', value: 'true' },
+					{ key: 'Access-Control-Allow-Origin', value: 'http://admin.lerp.io:3001' },
+					{ key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+					{ key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+				],
+			},
 		];
 	},
 	// Add any other Next.js configuration you need
