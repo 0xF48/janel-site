3a5ad0f665da6b26557a446aecad29117ed30b75
0xF48 <poofyiq@gmail.com>
2025-12-03 18:10:01 +0100

    update

--- app/pages/[slug]/page.tsx
+++ app/pages/[slug]/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { getData } from '../../getData';
-import { parseRawHtml } from '../../parseRawHtml';
 
 export async function generateStaticParams() {
 	const { pages } = await getData()
@@ -19,9 +18,9 @@ export default async function Page({ params }: any) {
 	return (
 
 		<div className='w-full flex flex-col md:flex-row gap-0 border-main-600 border-solid border-b-1'>
-			<div
-				className='mt-6 font-serif text-lg prose prose-invert max-w-none space-y-4'
-				dangerouslySetInnerHTML={{ __html: parseRawHtml(page.html) }} />
+			<div className='mt-6 font-serif text-lg prose prose-invert max-w-none space-y-4'>
+				{page.html}
+			</div>
 		</div >
 
 	);
